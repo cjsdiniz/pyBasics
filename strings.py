28f8adcee14ef12c4e3f2116c75e03078ae3c7c2
Carlos Diniz <67067863+cjsdiniz@users.noreply.github.com>
2023-04-18 11:00:14 +0100

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,8 @@
+print('Coding in Finance')
+
+print("An investment in knowledge pays the best interest.")
+
+print('Brian\'s mother: He\'s not an angel. He\'s a very naughty boy')
+
+# Newlines
+print('One\nTwo\nThree')
